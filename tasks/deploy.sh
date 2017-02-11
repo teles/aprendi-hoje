275f968e9b240afa12d646ffe60bb2b4a99f9e98
Jota Teles <josetelesmaciel@gmail.com>
2017-02-10 22:09:22 -0200

    agora vai [3]

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -4,6 +4,12 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="master"
 
+function doCompile {
+	mkdir temp
+	echo "{}" > temp/aprendizados.json
+	gulp
+}
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
@@ -18,30 +24,42 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO
-npm install
-mkdir temp
-echo "{}" > temp/aprendizados.json
-gulp
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Run our compile script
+doCompile
 
 # Now let's go have some fun with the cloned repo
+cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
-git commit -m "Adiciona novo aprendizado: ${SHA}"
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa_travis.pub -out id_rsa_travis -d
-chmod 600 id_rsa_travis.pub
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in oq-aprendi-hoje.enc -out oq-aprendi-hoje -d
+chmod 600 oq-aprendi-hoje
 eval `ssh-agent -s`
-ssh-add id_rsa_travis.pub
+ssh-add oq-aprendi-hoje
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
